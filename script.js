c5b9d8f89616a0e06d37140c9c581960bcdcdf51
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-07-18 02:02:09 +0100

    Put everything in a while loop and added aility to uit

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ var todos = [];
 
 var input = prompt("What would you to do today?");
 
-if(input === "list"){
-	console.log(todos); // prints all todos
-} else if(input === "new"){
-	// adds new todo by asking user
-	var newTodo = prompt("Enter new todo:");
-	// adds to array of todos
-	todos.push(newTodo);
+while(input !== "quit"){
+
+	// code that handles input
+	if(input === "list"){
+		console.log(todos); // prints all todos
+	} else if(input === "new"){
+		// adds new todo by asking user
+		var newTodo = prompt("Enter new todo:");
+		// adds to array of todos
+		todos.push(newTodo);
+	}
+
+	// asks again for input
+	input = prompt("What would you to do today?");
 }
+
+console.log("Cya later!");
