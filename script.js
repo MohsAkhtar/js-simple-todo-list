7601407faf3bcf999dc8c05bcfec8907889cc4ec
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-07-18 01:58:09 +0100

    Added add new todo

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ var input = prompt("What would you to do today?");
 
 if(input === "list"){
 	console.log(todos); // prints all todos
+} else if(input === "new"){
+	// adds new todo by asking user
+	var newTodo = prompt("Enter new todo:");
+	// adds to array of todos
+	todos.push(newTodo);
 }
